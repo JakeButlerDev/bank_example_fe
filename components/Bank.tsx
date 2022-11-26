355164870afd253e3880e7aca766ecfff0bf7e45
Jake Butler <JakeButler.student@careerdevs.com>
2022-11-25 21:11:26 -0500

    Updated css styling for Bank div, added flexbox extras

--- components/Bank.tsx
+++ components/Bank.tsx
@@ -24,12 +24,18 @@ const Bank: FC<{bank: BankProps}> = (props) => {
     return (
         <Fragment>
         <div style= {{
-            margin: "1em",
+            margin: "5em",
             display: "flex",
             flexDirection: "row",
             width: "100%"
         }}> 
-            <div>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "stretch",
+                alignSelf: "flex-start",
+                flexBasis: "100%"
+            }}>
                 <h1>{props.bank.id}</h1>
                 <p>{props.bank.name}</p>
                 <p>{props.bank.location}</p>
